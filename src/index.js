6c73f1ba2b62ddc310fc2d5d3421bb4e96e0f543
dinhhuy123 <huycholon93@gmail.com>
2023-04-19 10:29:05 +0700

    Complete Chapter 3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.scss'
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { fetchUsers } from './features/users/usersSlice';
+
+store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+    // <React.StrictMode>
     <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+        <App />
+    </Provider>,
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
