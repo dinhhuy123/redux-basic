c653094a832d73a672ed9dbfa3a1fb4b232f44b9
dinhhuy123 <huycholon93@gmail.com>
2023-04-21 08:42:44 +0700

    Complete Chapter 4

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,20 +1,11 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from './postsSlice';
+import { useSelector } from 'react-redux';
+import { selectAllPosts, getPostsStatus, getPostsError } from './postsSlice';
 import PostsExcerpt from './PostsExcerpt';
 
 function PostsList() {
-    const dispatch = useDispatch();
-
     const posts = useSelector(selectAllPosts);
     const postsStatus = useSelector(getPostsStatus);
     const error = useSelector(getPostsError);
-
-    useEffect(() => {
-        if (postsStatus === 'idle') {
-            dispatch(fetchPosts());
-        }
-    }, [postsStatus, dispatch]);
 
     let content;
     if (postsStatus === 'loading') {
@@ -26,12 +17,7 @@ function PostsList() {
         content = <p>{error}</p>;
     }
 
-    return (
-        <section>
-            <h2>Posts</h2>
-            {content}
-        </section>
-    );
+    return <section>{content}</section>;
 }
 
 export default PostsList;
